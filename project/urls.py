e4ccf36f468b3a033ba80696705f836d8b38f726
jtshulca <jenifer9420@hotmail.com>
2017-04-10 13:24:45 +0200

    Entrega ejercicio

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^(\d+)\+(\d+)$', "calc.views.suma"),
+    url(r'^(\d+)\-(\d+)$', "calc.views.resta"),
+    url(r'^(\d+)\*(\d+)$', "calc.views.multiplicacion"),
+    url(r'^(\d+)\/(\d+)$', "calc.views.dividision"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', "calc.views.error")
 )
